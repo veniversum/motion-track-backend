98f0ac764943505c3bdffdd47dad7cd3ddbfefbc
veniversum <hello@veniversum.me>
2016-07-29 18:06:28 +0800

    feat: sender & receiver matching

--- match.py
+++ match.py
@@ -1,62 +1,58 @@
 import pandas
 import statsmodels.api
 from numpy import *
-from flask import json
-from flask import Flask, request
-from flask import render_template
-from flask import jsonify
-app = Flask(__name__)
+from expiringdict import ExpiringDict
 
 data1 = None
 data2 = None
-result = {}
-
-@app.route('/granger/', methods=['POST'])
-def process():
-    content = request.get_json(silent=True)
-    y1 = content['y1']
-    y2 = content['y2']
-    try:
-        result = granger(y1, y2)
-        correff = corr(y1, y2)
-        return jsonify({"granger_match":result, "corrcoef":correff})
-    except Exception, e:
-        return jsonify({"granger_match":False, "corrcoef":0})
-
-@app.route('/log/', methods=['POST'])
-def logging():
-    data = request.get_json(silent=True)['data']
-    print data
-    global data1
-    global data2
-    global result
-    if data1 is None:
-        data1 = data
-    elif data2 is None:
-        data2 = data
-        result.update({"correff":corr(data1, data2)})
-        result.update({"granger_match":granger(data1, data2)})
-        result.update({"data1":data1})
-        result.update({"data2":data2})
-    else:
-        data1 = data
-        data2 = None
-    return ''
-
-@app.route('/status/', methods=['GET'])
-def status():
-    global result
-    return jsonify(result)
+senders = ExpiringDict(max_len=100, max_age_seconds=10)
+receivers = ExpiringDict(max_len=100, max_age_seconds=10)
+result = ExpiringDict(max_len=100, max_age_seconds=3600)
 
 def corr(y1, y2):
-    return corrcoef(y1, y2)[0][1]
+    return corrcoef(y1, y2, 0)[0][1]
 
-def granger(y1, y2):
+def granger(y1, y2, l = 20):
     model = statsmodels.tsa.vector_ar.var_model.VAR(array([y1,y2]).T)
     global data1
     global data2
-    results = model.fit(20)
-    return results.test_causality('y1', ['y2'], kind='wald')['conclusion'] == 'reject'
+    results = model.fit(maxlags=l, ic='aic')
+    return results.test_causality('y1', ['y2'], kind='f')['conclusion'] == 'reject'
 
-if __name__ == '__main__':
-    app.run(host= '0.0.0.0')
+def doMatch(uid, party):
+    global receivers
+    global senders
+
+    targets = None
+    selfdata = None
+    if party == 'sender':
+        targets = receivers
+        selfdata = senders.get(uid)['data']
+    elif party == 'receiver':
+        targets = senders
+        selfdata = receivers.get(uid)['data']
+    else:
+        return
+        
+    if selfdata is None:
+        return
+        
+    for otherUid, targetdict in targets.items():
+        targetdata = targetdict['data']
+        if granger(selfdata, targetdata):
+            info = {}
+            info.update('other_id', otherUid)
+            otherinfo = {}
+            otherinfo.update('other_id', uid)
+            if party == 'receiver':
+                info.update('amount', targetdict['amount'])
+                otherinfo.update('amount', targetdict['amount'])
+            else:
+                info.update('amount', senders.get(uid)['amount'])
+                otherinfo.update('amount', senders.get(uid)['amount'])
+            result.update(uid, info)
+            result.update(otherUid, otherinfo)
+            senders.pop(uid)
+            receivers.pop(uid)
+            senders.pop(otherUid)
+            receivers.pop(otherUid)
